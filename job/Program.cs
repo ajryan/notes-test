2d2fea9663474b4b58e148ef026d60c2d555333d
Aidan Ryan <ryan.aidan@gmail.com>
2014-04-20 09:04:04 -0700

    web job

---
+++ job/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace job
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Job run");
+            Trace.WriteLine("Job run");
+        }
+    }
+}
